b58a93fdba7dc85fc2a4661150dc10f54d28bf3d
Jørgen P. Tjernø <jorgenpt@gmail.com>
2021-02-07 17:46:17 -0800

    Add logging & --dry-run mode

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,35 @@
 mod bichrome_config;
 mod chrome_local_state;
 
-use std::env;
+use log::{error, info, trace, warn};
+use simplelog::*;
+use std::fs::File;
 use std::process::{Command, Stdio};
+use structopt::StructOpt;
 
-// Chrome Local State:
+// This is the definition of our command line options
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "bichrome",
+    about = "A program to pick Chrome profile based on the URL opened"
+)]
+struct Opt {
+    /// Use verbose logging
+    #[structopt(short, long)]
+    verbose: bool,
+    /// Use debug logging, even more verbose than --verbose
+    #[structopt(long)]
+    debug: bool,
+
+    /// Do not launch Chrome, just log what would've been launched
+    #[structopt(long)]
+    dry_run: bool,
+
+    /// List of URLs to open
+    urls: Vec<String>,
+}
+
+// Chrome Local State paths:
 //  - macOS path: "/Users/jorgenpt/Library/Application Support/Google/Chrome/Local State";
 //  - Windows path: r"C:\Users\jorgenpt\AppData\Local\Google\Chrome\User Data\Local State";
 
@@ -30,32 +55,70 @@ fn choose_profile<'a>(config: &'a bichrome_config::Configuration, url: &str) -> 
 const CHROME_EXE_PATH: &str = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
 
 fn main() {
+    // First parse our command line options, so we can use it to configure the logging.
+    let opt = Opt::from_args();
+    let log_level = if opt.debug {
+        LevelFilter::Trace
+    } else if opt.verbose {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    };
+
+    let mut loggers: Vec<Box<dyn SharedLogger>> = Vec::new();
+    // If we can write to bichrome.log, always use it.
+    if let Ok(file) = File::create("bichrome.log") {
+        loggers.push(WriteLogger::new(log_level, Config::default(), file));
+    }
+    // We only use the terminal logger in the debug build, since we don't allocate a console window otherwise.
+    if cfg!(debug_assertions) {
+        if let Some(logger) = TermLogger::new(log_level, Config::default(), TerminalMode::Mixed) {
+            loggers.push(logger)
+        }
+    };
+
+    CombinedLogger::init(loggers).unwrap();
+    trace!("command line options: {:?}", opt);
+
+    // TODO: Set up the appropriate registry entries to appear on a new install.
     // TODO: Use profiles + a distribution config to generate bichrome_config if it doesn't exist.
     // TODO: Read profile from %localappdata%.
+
+    // We try to read the config, and otherwise just use an empty one instead.
     let config = bichrome_config::read_config_from_file("bichrome_config.json");
     let config = match config {
         Ok(config) => {
-            println!("loaded config {:#?}", config);
+            trace!("config: {:#?}", config);
             config
         }
         Err(e) => {
-            println!("failed to parse config: {:?}", e);
-            println!("opening URLs without profile");
+            error!("failed to parse config: {:?}", e);
+            warn!("opening URLs without profile");
             bichrome_config::Configuration::empty()
         }
     };
 
-    for url in env::args().skip(1) {
-        let mut chrome = Command::new(CHROME_EXE_PATH);
-        chrome
-            .stdout(Stdio::null())
-            .stdin(Stdio::null())
-            .stderr(Stdio::null());
-
+    for url in opt.urls {
+        let mut args = Vec::new();
         if let Some(profile_name) = choose_profile(&config, &url) {
-            chrome.arg(format!("--profile-directory={}", profile_name));
+            args.push(format!("--profile-directory={}", profile_name));
         }
+        args.push(url);
 
-        chrome.arg(url).spawn().unwrap();
+        if opt.dry_run {
+            info!(
+                "launching \"{}\" \"{}\"",
+                CHROME_EXE_PATH,
+                args.join("\" \"")
+            );
+        } else {
+            Command::new(CHROME_EXE_PATH)
+                .stdout(Stdio::null())
+                .stdin(Stdio::null())
+                .stderr(Stdio::null())
+                .args(args)
+                .spawn()
+                .unwrap();
+        }
     }
 }
