449bc6ebc225b990df6b2207cb11101a65701d80
Jørgen P. Tjernø <jorgenpt@gmail.com>
2021-02-13 18:04:15 -0800

    Attempt to do subsystem stuff in windows.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 #![deny(clippy::all)]
-// We use the console subsystem in debug builds, but use the Windows subsystem in release
-// builds so we don't have to allocate a console and pop up a command line window.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![cfg_attr(debug_assertions, windows_subsystem = "console")]
 
 #[cfg_attr(target_os = "windows", path = "windows.rs")]
 mod os;
